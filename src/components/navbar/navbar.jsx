a926b4926d4bafa0c9de9929988de4c55e0f853c
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-19 15:33:18 +0500

    nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,3 +1,19 @@
-export const NavBar  = () =>{
-    
-}
+export const NavBar = () => {
+  return (
+    <div className="container mx-auto   ">
+      <div className="desktop-nav-container sticky p-6 mt-4  rounded-4xl bg-[#120F1F]    ">
+        <nav>
+          <ul className="flex flex-row text-white font-bold text-lg justify-center gap-36">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li className="text-xl text-purple-400 ">Saad Bin Khalid</li>
+            <li>Resume</li>
+            <li>Portfolio</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
