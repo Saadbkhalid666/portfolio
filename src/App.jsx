c0ce2b2a2fbb3472249af83210762954217477f1
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-20 22:11:45 +0500

    today's changes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { FaChevronCircleUp } from "react-icons/fa"
 import { NavBar } from "./components/navbar/navbar"
 import { Hero } from "./sections/hero/hero"
 
 function App() {
   
-  return <div className="">
+  return <div className="relative">
     <NavBar   /> 
     <Hero    /> 
 
- 
+ <FaChevronCircleUp   className="fixed right-10 bottom-10 animate-pulse text-purple-600  hover:scale-110 transition-all duration-150 ease-in-out hover:cursor-pointer drop-shadow-[0px_0px_10px_purple]  text-4xl" />
   </div>
 
 }
