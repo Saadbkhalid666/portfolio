9fd2d065d1551ff34168f4fcafcdc16d32274dc0
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-20 20:49:10 +0500

    import/export

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { NavBar } from "./components/navbar/navbar"
+import { Hero } from "./sections/hero/hero"
 
 function App() {
   
   return <div className="">
     <NavBar   /> 
-     
+    <Hero    /> 
 
  
   </div>
