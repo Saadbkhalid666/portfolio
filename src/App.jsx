5a2f7fb59205053431284a096392411b872cf365
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-21 12:33:42 +0500

    Animated List

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { NavBar } from "./components/navbar/navbar";
 import { Hero } from "./sections/hero/hero";
  
 import { StatsNAcheivments } from "./sections/stats/Stats";
+import { Services } from "./sections/services/Service";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <NavBar />
       <Hero />
 <StatsNAcheivments   />
+
+<Services    />
       <FaChevronCircleUp className="fixed right-10 bottom-10 animate-pulse text-purple-600  hover:scale-110 transition-all duration-150 ease-in-out hover:cursor-pointer drop-shadow-[0px_0px_10px_purple]  text-4xl" />
 
 
---
+++ src/sections/services/Service.jsx
@@ -0,0 +1,8 @@
+export const Services = () => {
+    return <div className="container">
+        <h1>My Quality Services</h1>
+        <div className="content-container">
+
+        </div>
+    </div>
+}
