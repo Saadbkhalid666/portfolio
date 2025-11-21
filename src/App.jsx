29d7e556146eaa8bee8471ca5a1417704b1f0781
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-21 11:05:10 +0500

    CountUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { FaChevronCircleUp } from "react-icons/fa"
-import { NavBar } from "./components/navbar/navbar"
-import { Hero } from "./sections/hero/hero"
+import { FaChevronCircleUp } from "react-icons/fa";
+import { NavBar } from "./components/navbar/navbar";
+import { Hero } from "./sections/hero/hero";
+import CountUp from "./components/CountUp/CountUp";
 
 function App() {
-  
-  return <div className="relative">
-    <NavBar   /> 
-    <Hero    /> 
+  return (
+    <div className="relative">
+      <NavBar />
+      <Hero />
 
- <FaChevronCircleUp   className="fixed right-10 bottom-10 animate-pulse text-purple-600  hover:scale-110 transition-all duration-150 ease-in-out hover:cursor-pointer drop-shadow-[0px_0px_10px_purple]  text-4xl" />
-  </div>
+      <FaChevronCircleUp className="fixed right-10 bottom-10 animate-pulse text-purple-600  hover:scale-110 transition-all duration-150 ease-in-out hover:cursor-pointer drop-shadow-[0px_0px_10px_purple]  text-4xl" />
 
+
+
+<CountUp
+  from={0}
+  to={100}
+  separator=","
+  direction="up"
+  duration={1}
+  className="count-up-text"
+/>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
