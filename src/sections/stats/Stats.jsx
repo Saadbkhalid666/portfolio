39128d0d262dfebfcf65349bfc136ed649741367
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-21 11:53:11 +0500

    stats

--- src/sections/stats/Stats.jsx
+++ src/sections/stats/Stats.jsx
@@ -1,50 +1,41 @@
 import CountUp from "../../components/CountUp/CountUp";
+
 export const StatsNAcheivments = () => {
   return (
-    <div className="container text-white h-screen w-full flex flex-row justify-around mx-auto  text-6xl font-extrabold">
-      <div className="contributions ">
-        <CountUp
-          from={0}
-          to={1000}
-          direction="up"
-          duration={1}
-          className="count-up-text inline text-white "
-        />
-        <span>+</span>
-        <p className="text-4xl"> Code Contributions</p>
-      </div>
-      <div className="projects">
-        <CountUp
-          from={0}
-          to={8}
-          direction="up"
-          duration={0.1}
-          className="count-up-text text-white "
-        />
-        <span>+</span>
-        <p className="text-4xl">Projects</p>
-      </div>
-      <div className="stack">
-        <CountUp
-          from={0}
-          to={20}
-          direction="up"
-          duration={1}
-          className="count-up-text text-white "
-        />
-        <span>+</span>
-        <p className="text-4xl">Tech Stack Skills</p>
-      </div>
-      <div className="followers">
-        <CountUp
-          from={0}
-          to={650}
-          direction="up"
-          duration={1}
-          className="count-up-text text-white "
-        />
-        <span>+</span>
-        <p className="text-4xl">Followers</p>
+    <div className="container mx-auto text-white py-10   ">
+      <div className="
+        grid 
+        grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 
+        gap-10 
+        place-items-center 
+        text-center
+        font-extrabold
+      ">
+        
+        <div>
+          <CountUp from={0} to={1000} direction="up" duration={1} className="count-up-text inline text-white lg:text-6xl " />
+          <span className="lg:text-6xl">+</span>
+          <p className="md:text-2xl lg:text-4xl">Code Contributions</p>
+        </div>
+
+        <div>
+          <CountUp from={0} to={8} direction="up" duration={0.1} className="count-up-text inline text-white lg:text-6xl" />
+          <span className="lg:text-6xl">+</span>
+          <p className="md:text-2xl lg:text-4xl">Projects</p>
+        </div>
+
+        <div>
+          <CountUp from={0} to={20} direction="up" duration={1} className="count-up-text inline text-white lg:text-6xl" />
+          <span className="lg:text-6xl">+</span>
+          <p className="md:text-2xl lg:text-4xl">Tech Stack Skills</p>
+        </div>
+
+        <div>
+          <CountUp from={0} to={650} direction="up" duration={1} className="count-up-text inline text-white lg:text-6xl" />
+          <span className="lg:text-6xl">+</span>
+          <p className="md:text-2xl lg:text-4xl">Followers</p>
+        </div>
+
       </div>
     </div>
   );
