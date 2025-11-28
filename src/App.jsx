c7e3432587d8803752f9baa22ff79321b0291e2a
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-28 21:42:40 +0500

    Cards.jsx n Cards.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { FaChevronCircleUp } from "react-icons/fa";
 import { NavBar } from "./components/navbar/navbar";
 import { Hero } from "./sections/hero/hero";
- 
+
 import { StatsNAcheivments } from "./sections/stats/Stats";
 import { Services } from "./sections/services/Service";
 
@@ -10,15 +10,10 @@ function App() {
     <div className="relative">
       <NavBar />
       <Hero />
-<StatsNAcheivments   />
+      <StatsNAcheivments />
 
-<Services    />
+      <Services />
       <FaChevronCircleUp className="fixed right-10 bottom-10 animate-pulse text-purple-600  hover:scale-110 transition-all duration-150 ease-in-out hover:cursor-pointer drop-shadow-[0px_0px_10px_purple]  text-4xl" />
-
-
-
-
-
     </div>
   );
 }
---
+++ src/components/cards/cards.jsx
@@ -0,0 +1,12 @@
+import "./cards.css"
+const Cards = () => {
+    return  <div class="card">
+  <p class="heading">
+    Popular this month
+  </p>
+  <p>
+    Powered By
+  </p>
+  <p>Uiverse
+</p></div>
+}
