7653d48209af5465b2abcd5e3935abaecbd975b5
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-30 15:31:41 +0500

    project file

--- src/sections/portfolio/portfolio.jsx
+++ src/sections/portfolio/portfolio.jsx
@@ -2,7 +2,7 @@
 export const Portfolio = () => {
 return <section id="portfolio" className="h-screen w-screen mt-[10%]">
 
-<h1 className=" bg-clip-text bg-linear-to-r text-transparent from-purple-800 via-purple-300 to-white  text-6xl text-center font-extrabold font-serif ">My Quality Work</h1>
+<h1 className=" bg-clip-text bg-linear-to-r text-transparent from-purple-800 via-purple-300 to-white  text-6xl text-center font-extrabold font-serif h-full">My Quality Work</h1>
 
 
 </section>     
