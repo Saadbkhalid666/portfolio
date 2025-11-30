02b4eef453c1531a8944a9f08e58b81b8604febc
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-11-30 16:10:21 +0500

    cards.jsx

--- src/sections/portfolio/portfolio.jsx
+++ src/sections/portfolio/portfolio.jsx
@@ -1,9 +1,14 @@
+import { Card } from "../../components/cards/cards"
 
 export const Portfolio = () => {
 return <section id="portfolio" className="h-screen w-screen mt-[10%]">
 
 <h1 className=" bg-clip-text bg-linear-to-r text-transparent from-purple-800 via-purple-300 to-white  text-6xl text-center font-extrabold font-serif h-full">My Quality Work</h1>
-
+<Card  title="My Portfolio Card"
+  description="This is a sample card built with React + Tailwind. Elegant and reusable."
+  skills={["HTML", "CSS", "JavaScript", "React", "TS"]}
+  link="https://github.com/your-repo"/>
 
 </section>     
 }
+
