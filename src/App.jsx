110a35d72a2836630030532ab1ced57acaadc40e
Saad Bin Khalid <saadbkhalid666@gmail.com>
2025-12-01 18:48:51 +0500

    education

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Services } from "./sections/services/Service";
 import { Portfolio } from "./sections/portfolio/portfolio";
 import { Skills } from "./sections/skills/skills";
 import { AchievementsSlider } from "./sections/Achievements/Achievements";
+import { EducationExperience } from "./sections/educations/Education";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Portfolio    />
       <Skills   />
       <AchievementsSlider    />
+      <EducationExperience   />
       <FaChevronCircleUp className="fixed right-10 bottom-10 animate-pulse text-purple-600  hover:scale-110 transition-all duration-150 ease-in-out hover:cursor-pointer drop-shadow-[0px_0px_10px_purple]  text-4xl" />
     </div>
   );
