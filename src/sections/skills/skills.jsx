c0c2efaf0fc0dacf46730e6423b3ede2b076f409
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-04-13 22:36:27 -0700

    changes in skill section

--- src/sections/skills/skills.jsx
+++ src/sections/skills/skills.jsx
@@ -44,13 +44,13 @@ export const Skills = () => {
   }, []);
 
   return (
-    <section id="resume" className="py-16 text-white">
+    <section id="resume" className=" text-white">
       <h1 className="bg-clip-text text-transparent
         bg-linear-to-r to-purple-800 via-purple-300 from-white
         text-4xl sm:text-5xl md:text-6xl 
-        text-center font-extrabold font-serif overflow-hidden mb-12">My Tech Stack</h1>
-
-      <div ref={containerRef} className="container grid grid-cols-3 lg:grid-cols-6 gap-10">
+        text-center font-extrabold font-serif overflow-hidden mb-6">My Tech Stack</h1>
+<p className="text-center text-lg mb-12 text-gray-400">The skills, tools and technologies I am really good at:</p>
+      <div ref={containerRef} className="container mx-auto grid grid-cols-3 lg:grid-cols-6 xl:grid-cols-8 gap-10">
         {/* ===== LANGUAGES ===== */}
         <SkillCard className="skill-card" title="HTML" color="text-[#ff6b35]" icon={<FaHtml5 />} />
         <SkillCard className="skill-card" title="CSS" color="text-[#2563eb]" icon={<FaCss3 />} />
@@ -58,7 +58,7 @@ export const Skills = () => {
         <SkillCard className="skill-card" title="TypeScript" color="text-[#3b82f6]" icon={<SiTypescript />} />
         <SkillCard className="skill-card" title="Python" color="text-[#eab308]" icon={<FaPython />} />
         <SkillCard className="skill-card" title="C Language" color="text-[#06b6d4]" icon={<SiC />} />
-        <SkillCard className="skill-card" title="Dart" color="text-[#38bdf8]" icon={<SiDart />} />
+        <SkillCard className="skill-card" title="ReactNative" color="text-[#38bdf8]" icon={<SiReact />} />
 
         {/* ===== DATABASES ===== */}
         <SkillCard className="skill-card" title="MongoDB" color="text-[#22c55e]" icon={<SiMongodb />} />
@@ -73,17 +73,8 @@ export const Skills = () => {
         {/* ===== FRONTEND ===== */}
         <SkillCard className="skill-card" title="React" color="text-[#67e8f9]" icon={<SiReact />} />
         <SkillCard className="skill-card" title="Angular" color="text-[#ef4444]" icon={<SiAngular />} />
-        <SkillCard className="skill-card" title="Flutter" color="text-[#60a5fa]" icon={<SiFlutter />} />
         <SkillCard className="skill-card" title="TailwindCSS" color="text-[#38bdf8]" icon={<SiTailwindcss />} />
 
-        {/* ===== VERSION CONTROL ===== */}
-        <SkillCard className="skill-card" title="Git" color="text-[#ef4444]" icon={<SiGit />} />
-        <SkillCard className="skill-card" title="GitHub" color="text-white" icon={<SiGithub />} />
-
-        {/* ===== TOOLS ===== */}
-        <SkillCard className="skill-card" title="VS Code" color="text-[#3b82f6]" icon={<VscVscodeInsiders />} />
-        <SkillCard className="skill-card" title="Postman" color="text-[#f97316]" icon={<SiPostman />} />
-        <SkillCard className="skill-card" title="ChatGPT" color="text-[#fff]" icon={<PiOpenAiLogo />} />
       </div>
     </section>
   );
