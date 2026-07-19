adf634bc1c864576a27f70b3aaa7b2704c726479
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-19 12:32:10 +0500

    hero.js

--- src/app/sections/Hero.js
+++ src/app/sections/Hero.js
@@ -9,13 +9,10 @@ export const Hero = () => {
       </div>
 
       <div className="relative z-10 h-full max-w-7xl mx-auto px-8 grid grid-cols-3 items-center">
-        
         <div className="flex flex-col justify-center">
-          <p className="text-gray-400 text-lg mb-2">
-            Hello I&apos;m
-          </p>
+          <p className="text-purple-400 text-lg mb-2 ">Hello I&apos;m</p>
 
-          <h1 className="text-6xl font-bold text-[#f5f5f7] ">
+          <h1 className="text-6xl font-bold text-[#f5f5f7] font-ancizar ">
             Saad
             <br />
             Bin Khalid
@@ -24,14 +21,18 @@ export const Hero = () => {
 
         <div></div>
 
-        <div className="flex justify-end">
-          <h2 className="text-5xl font-semibold  mt-12 text-right">
-            Full-Stack
-            <br />
-            Engineer
+        <div className="flex justify-end mt-30">
+          <h2 className=" leading-none">
+<p className="text-[#6e6e73] font-instrument ">01 - Role</p>
+            <span className="font-ancizar block text-5xl font-semibold bg-linear-to-b from-white to-black bg-clip-text text-transparent">
+              Full-Stack
+            </span>
+
+            <span className="text-right font-ancizar block text-purple-300 text-5xl font-semibold -mt-4 mr-2">
+              Engineer
+            </span>
           </h2>
         </div>
-
       </div>
     </section>
   );
