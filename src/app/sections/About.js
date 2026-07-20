ceeeda401adaed64ff9cc92dd07ba656d2193cb2
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-20 18:16:50 +0500

    font lily added in about.js

--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -1,16 +1,32 @@
-import Image from "next/image"
-import myimg from "../assets/myimg.jpg"
+import Image from "next/image";
+import myimg from "../assets/myimg.jpg";
 
 export const About = () => {
-    return <div className="container">
+  return (
+    <div className="mt-30">
+      <div className="container flex flex-row mx-auto max-w-7xl ">
         <div className="content-container">
-            <h1 className="">Transforming <br /> Ideas Daily;</h1>
-            <p>I'm a Full Stack Engineer building modern, scalable, high-performance digital products - from responsive web apps and mobile apps to AI-powered conversational systems. My core stack is built around Node.js, Python (flask & Django), React/Nextjs and AI integerations.<br /> 
-            Good software isn't just clean code - it solves real business problems, feels effortless to use, and holds up as it scales, that's the standard i Build to: performance and thoughtful design in every project. <br />
-            That's the work i do - and the value i bring. </p>
+          <h1 className="text-[#f5f5f7] text-6xl font-lily ">
+            Transforming <br /> Ideas Daily;
+          </h1>
+          <p className="text-[#6e6e73]">
+            I'm a Full Stack Engineer building modern, scalable,
+            high-performance digital products - from responsive web apps and
+            mobile apps to AI-powered conversational systems. My core stack is
+            built around Node.js, Python (flask & Django), React/Nextjs and AI
+            integerations.
+            <br />
+            Good software isn't just clean code - it solves real business
+            problems, feels effortless to use, and holds up as it scales, that's
+            the standard i Build to: performance and thoughtful design in every
+            project. <br />
+            That's the work i do - and the value i bring.{" "}
+          </p>
         </div>
         <div className="image-container">
-            <Image   className="w-60" src={myimg} alt="developer image" />
+          <Image className="w-60" src={myimg} alt="developer image" />
         </div>
+      </div>
     </div>
-}
+  );
+};
