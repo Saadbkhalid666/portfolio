8df9138104eb45483c8f4697a458f4a0957efdef
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-20 18:07:16 +0500

    my image is added

--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -2,6 +2,9 @@ export const About = () => {
     return <div className="container">
         <div className="content-container">
             <h1 className="">Transforming <br /> Ideas Daily;</h1>
+            <p>I'm a Full Stack Engineer building modern, scalable, high-performance digital products - from responsive web apps and mobile apps to AI-powered conversational systems. My core stack is built around Node.js, Python (flask & Django), React/Nextjs and AI integerations.<br /> 
+            Good software isn't just clean code - it solves real business problems, feels effortless to use, and holds up as it scales, that's the standard i Build to: performance and thoughtful design in every project. <br />
+            That's the work i do - and the value i bring. </p>
         </div>
         <div className="image-container"></div>
     </div>
